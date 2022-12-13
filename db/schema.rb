8b8cdf8d1b579bc6d39484ad87143c0a0a9049d4
Gabriel <gopxfs@hotmail.com>
2022-12-13 03:36:57 -0300

    Add friendship table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_13_060805) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_13_063312) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_13_060805) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["likes_id"], name: "index_comments_on_likes_id"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "friend_id"
+    t.boolean "accepted", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -62,6 +71,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_13_060805) do
   end
 
   add_foreign_key "comments", "likes", column: "likes_id"
+  add_foreign_key "friendships", "users"
   add_foreign_key "posts", "comments", column: "comments_id"
   add_foreign_key "posts", "likes", column: "likes_id"
   add_foreign_key "users", "posts", column: "posts_id"
