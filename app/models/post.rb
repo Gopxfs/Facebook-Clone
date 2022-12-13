27431f68f498da93d8932f6e8b5cbebe2b2a5a02
Gabriel <gopxfs@hotmail.com>
2022-12-13 03:27:00 -0300

    Add models & validations

---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ApplicationRecord
+  has_many :likes, dependent :destroy
+  has_many :comments, dependent :destroy
+
+  validates :title, presence: true, length: { minimum: 1, maximum: 100 }
+  validates :text, presence: true, length: { minimum: 1, maximum: 1000 }
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :posts, dependent: :destroy
+
+  validates :first_name, presence: true, length: { minimum: 1, maximum: 50 }
+  validates :last_name, presence: true, length: { minimum: 1, maximum: 50 }
+  validates :bio, presence: true, length: { minimum: 1, maximum: 1000 }
 end
