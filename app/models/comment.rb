d852603cef6cb92f4d53cd0681df50d69ad698dd
Gabriel <gopxfs@hotmail.com>
2022-12-13 03:36:34 -0300

    Fix typo

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-  has_many :likes, dependent: destroy
+  has_many :likes, dependent: :destroy
   validates :text, presence: true, length: { minimum: 1, maximum: 1000 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  has_many :likes, dependent :destroy
-  has_many :comments, dependent :destroy
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: { minimum: 1, maximum: 100 }
   validates :text, presence: true, length: { minimum: 1, maximum: 1000 }
